a61d5fe59fec01a709bd0c67fc1a2fd0d4b30010
Eamon Walsh <eamongwalsh@gmail.com>
2016-03-06 00:31:09 +0000

    Add NY Times articles using NYTimes developer API key

--- js/script.js
+++ js/script.js
@@ -23,7 +23,24 @@ function loadData() {
     var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
     
+    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=d6a398f54661238a6baf846544453696:2:74631104';
 
+    $.getJSON(nytimesUrl, function (data) {
+        // console.log(data);
+        
+        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+        
+        articles = data.response.docs;
+        for (var i=0; i<articles.length; i++) {
+            var article = articles[i];
+            $nytElem.append('<li class="article">'+
+                '<a href="'+article.web_url+'">'+article.headline.main+
+                    '</a>'+
+                '<p>' + article.snippet + '</p>'+'</li>');
+        };
+        
+    });
+    
     return false;
 };
 
